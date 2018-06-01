c43efa5371e26dd99a2372fb16c0e9f2a751f543
zoemorgan0516 <zoemorgan0516@gmail.com>
2018-06-01 17:00:34 +0800

    增加软删除测试

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -51,4 +51,70 @@ RSpec.describe Article, type: :model do
     expect(@article.errors[:title].last).to eq "is too short (minimum is 5 characters)" 
     end
   end
+  describe '软删除' do
+    before :each do
+      @user = create(:user)
+      @article = create(:article, user: @user)
+      @user1 = create(:user, name: 'fwl', email: 'aaa@lindo.io', password: '123123')
+      @article1 = create(:article, user: @user1)
+      @article2 = create(:article, user: @user1)
+    end
+ 
+    it '查看回收站' do
+    # 没有删除文章时 回收站为空
+      expect(Article.deleted.count).to eq 0
+    end
+
+    it '删除一条文章' do
+      @article.destroy
+      expect(Article.count).to eq 2
+      expect(Article.deleted.count).to eq 1
+      expect(Article.deleted).to include @article
+    end
+
+    it '删除多条文章' do
+      @article.destroy
+      @article1.destroy
+      expect(Article.count).to eq 1
+      expect(Article.deleted.count).to eq 2
+      expect(Article.deleted).to include @article, @article1 
+    end
+
+    it '回收站删除文章' do
+      @article.destroy!
+      expect(Article.count).to eq 2
+      expect(Article.deleted.count).to eq 0
+      @article1.destroy!
+      expect(Article.count).to eq 1
+      expect(Article.deleted.count).to eq 0
+    end
+
+    it '回收站文章恢复' do
+      @article.destroy
+      @article1.destroy
+      @article2.destroy
+      @article.restore
+      @article1.restore
+      @article2.restore
+      expect(Article.count).to eq 3
+      expect(Article.all).to include @article, @article1, @article2 
+      expect(Article.deleted.count).to eq 0
+    end
+
+    it '文章首页排序' do
+      expect(@article.position).to eq 1
+      expect(@article1.position).to eq 2
+      expect(@article2.position).to eq 3
+    end
+
+    # it '文章上移' do
+    #   @article1.move_higher
+    #   @article1.move_higher
+    #   @article1.move_higher
+    #   expect(@article1.position).to eq 1
+    #   @article2.move_higher
+    #   expect(@article1.position).to eq 1
+    # end
+
+  end
 end
